14bfd974071d5a29eb1efe0cd2ae85a092c137ad
bl4ckswan <bl4cksw4n.1337@gmail.com>
2022-05-12 19:13:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const contractAddress = "0xA132E18Bea68A9657DcA6EDd6334E8f8980e9255"
+  export const contractAddress = "0x2A87e6CbD433F988EE4E2574CD2Be757647454d1"
   export const ownerAddress = "0x6C784623B0f04D22F4f3a899c817566aA5c57fd5"
   
